aac8459d4c88377a4b7cac8fa96649976a751ccb
JuanJUribe <juanjouribev1@gmail.com>
2019-10-02 11:21:31 -0700

    everyone_sign() finished, now it considers duplicates and uses enumerate to iterate

--- cards/cards.py
+++ cards/cards.py
@@ -2,10 +2,10 @@ import sys
 
 def everyone_sign(names):
     if type(names) != list:
-        print('everyone_sign function requires a list')
-        sys.exit()
+        raise ValueError('everyone_sign function requires a list as argument')
+    names = list(set(names))
     messages = {}
-    for i, recipient in names:
+    for i, recipient in enumerate(names):
         friends = ', '.join(names[:i]+names[i+1:])
         message = 'Thank you! Your friends {}'.format(friends)
         messages.update({recipient : message})
